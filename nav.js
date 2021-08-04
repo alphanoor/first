4c87e56af6982ab94e4105dfdae01b504bd17161
Noor <alphaenlightner@gmail.com>
2021-08-04 23:32:05 +0530

    ddd

--- nav.js
+++ nav.js
@@ -1,14 +1,14 @@
 const path = require('path');
 const express = require('express');
 const app = express();
-
+app.set('view engine', 'pug');
 //const hbs = require('hbs');
 const port = process.env.PORT || 3000;
-//const staticPath = path.join(__dirname, "../express/public");
+const staticPath = path.join(__dirname, "../express/public");
 //console.log(path.join(__dirname, "../express/public"));
-//app.use(express.static(staticPath));
+app.use(express.static(staticPath));
 app.get("/", (req, res) => {
-    res.sendFile('index.html', { root: path.join(__dirname, "../express/public") });
+    res.send('index');
 })
 app.listen(port, () => {
     console.log(`listening to the port at ${port}`);
