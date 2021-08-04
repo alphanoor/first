52a7f4ccc3a028f509dd7bdbe4ba9aabca64693e
Noor <alphaenlightner@gmail.com>
2021-08-04 23:00:11 +0530

    hello

--- nav.js
+++ nav.js
@@ -7,9 +7,9 @@ const port = process.env.PORT || 3000;
 const staticPath = path.join(__dirname, "../express/public");
 //console.log(path.join(__dirname, "../express/public"));
 app.use(express.static(staticPath));
-// app.get("/", (req, res) => {
-//     res.send("enjoy");
-// })
+app.get("/", (req, res) => {
+    res.send("index");
+})
 app.listen(port, () => {
     console.log(`listening to the port at ${port}`);
 });
